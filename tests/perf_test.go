a076bdd38da98ad89d60655388a9c3560331d598
Jamie Alquiza <jamiealquiza@gmail.com>
2017-05-29 11:49:35 -0600

    include moved tests

---
+++ tests/perf_test.go
@@ -0,0 +1,106 @@
+package vaporhr_test
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"testing"
+
+	"github.com/jamiealquiza/vaporhr"
+)
+
+var (
+	filePath string = "./words.txt"
+)
+
+// Words via https://raw.githubusercontent.com/dwyl/english-words/master/words.txt.
+
+func TestDistribution(t *testing.T) {
+	r, _ := vaporhr.New(&vaporhr.Config{})
+
+	nodes := map[string]uint64{
+		"node-a": 0,
+		"node-b": 0,
+		"node-c": 0,
+		"node-d": 0,
+		"node-e": 0,
+	}
+
+	for n := range nodes {
+		r.AddNode(n)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		nodes[r.Get(scanner.Text())]++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Node:key-count - %v\n", nodes)
+
+	var total float64
+	var counts []float64
+	for k := range nodes {
+		v := float64(nodes[k])
+		total += v
+		counts = append(counts, v)
+	}
+
+	sort.Float64s(counts)
+	rng := counts[len(counts)-1] - counts[0]
+	imbp := rng / total * 100
+	imbr := counts[len(counts)-1] / counts[0]
+
+	fmt.Printf("Greatest imbalance - portion of keys: %.2f%% / ratio: %.2fx\n\n",
+		imbp, imbr)
+
+}
+
+func BenchmarkGet(b *testing.B) {
+	b.StopTimer()
+
+	keyCount := 200000
+	mod := keyCount - 1
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(file)
+	r, _ := vaporhr.New(&vaporhr.Config{
+		Nodes: []string{"node-a", "node-b", "node-c", "node-d", "node-e"},
+	})
+
+	keys := []string{}
+	for scanner.Scan() {
+		k := scanner.Text()
+		keys = append(keys, k)
+		if len(keys) == keyCount {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		r.Get(keys[i%mod])
+	}
+}
