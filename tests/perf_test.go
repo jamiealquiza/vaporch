0aa941eb26920eb0d89d918a0c691a1f1b869f4f
Jamie Alquiza <jamiealquiza@gmail.com>
2017-06-12 08:32:09 -0600

    tests at various vnode factors

--- tests/perf_test.go
+++ tests/perf_test.go
@@ -17,53 +17,56 @@ var (
 // Words via https://raw.githubusercontent.com/dwyl/english-words/master/words.txt.
 
 func TestDistribution(t *testing.T) {
-	r, _ := vaporch.New(&vaporch.Config{})
+	// Test at various vnode factors.
+	for i := 1; i <= 3; i++ {
+		nodes := map[string]uint64{
+			"node-a": 0,
+			"node-b": 0,
+			"node-c": 0,
+			"node-d": 0,
+			"node-e": 0,
+		}
 
-	nodes := map[string]uint64{
-		"node-a": 0,
-		"node-b": 0,
-		"node-c": 0,
-		"node-d": 0,
-		"node-e": 0,
+		r, _ := vaporch.New(&vaporch.Config{VNodes: i})
+		for n := range nodes {
+			r.AddNode(n)
+		}
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			nodes[r.Get(scanner.Text())]++
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("VNode factor: %d\n", i)
+		fmt.Printf("\t[Node:key-count]\t%v\n", nodes)
+
+		var total float64
+		var counts []float64
+		for k := range nodes {
+			v := float64(nodes[k])
+			total += v
+			counts = append(counts, v)
+		}
+
+		sort.Float64s(counts)
+		rng := counts[len(counts)-1] - counts[0]
+		imbp := rng / total * 100
+		imbr := counts[len(counts)-1] / counts[0]
+
+		fmt.Printf("\t[Greatest imbalance]\tportion of keys: %.2f%% / ratio: %.2fx\n\n",
+			imbp, imbr)
 	}
-
-	for n := range nodes {
-		r.AddNode(n)
-	}
-
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		nodes[r.Get(scanner.Text())]++
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Node:key-count - %v\n", nodes)
-
-	var total float64
-	var counts []float64
-	for k := range nodes {
-		v := float64(nodes[k])
-		total += v
-		counts = append(counts, v)
-	}
-
-	sort.Float64s(counts)
-	rng := counts[len(counts)-1] - counts[0]
-	imbp := rng / total * 100
-	imbr := counts[len(counts)-1] / counts[0]
-
-	fmt.Printf("Greatest imbalance - portion of keys: %.2f%% / ratio: %.2fx\n\n",
-		imbp, imbr)
 
 }
 
